2cf36b46ffa4116916db0cb41ae57fbd089529ea
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-12-24 04:09:10 -0500

    Added vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import { Analytics } from "@vercel/analytics/react"
 
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"],
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
       <body className={jetbrainsMono.variable}>
         <Header />
         <StairTransition />
-        <PageTransition>{children}</PageTransition>
+        <PageTransition>{children} <Analytics debug={true} /></PageTransition>
 
       </body>
     </html>
